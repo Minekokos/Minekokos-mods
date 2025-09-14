812a034a4330e2d3e34a5e1ce1d54020e6e29592
Minekokos <97981208+Minekokos@users.noreply.github.com>
2025-09-14 14:21:52 +0200

    14.09.25.01

--- PathOfTheMonq/Items/Consumables/Duck_lock.cs
+++ PathOfTheMonq/Items/Consumables/Duck_lock.cs
@@ -5,13 +5,30 @@ using Microsoft.Xna.Framework;
 
 namespace PathOfTheMonq.Items.Consumables
 {
-    public class Duck_lock
+    public class Duck_lock : ModItem
     {
         public override void SetDefaults()
         {
             Item.width = 18;
             Item.height = 18;
-            Item.useStyle = ItemUseStyleID.swing;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.rare = ItemRarityID.Cyan;
+            Item.sellPrice(silver = 1);
         }
+
+        public override bool CanUseItem(Player player)
+        {
+            return !NPC.anyNPCs(ModContent.NPCType<Doosh_HAn_body>);
+        }
+
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+            .AddIngredient(ItemID.Duck)
+            .AddIngredient(ItemID.Lock)
+            .AddTile(TileID.DemonAltars)
+            .Register();
+        } 
     }
 }
